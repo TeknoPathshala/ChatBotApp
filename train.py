3a6f21202dbd01c28bfe9d32d76560328eb3e2ee
TeknoPathshala <95009959+TeknoPathshala@users.noreply.github.com>
2023-09-28 10:26:51 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 # Import necessary libraries
 import random
-from tensorflow.keras.optimizers import SGD
+import tensorflow as tf
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
 import numpy as np
@@ -9,87 +9,52 @@ import json
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-# Initialize WordNetLemmatizer from NLTK
-lemmatizer = WordNetLemmatizer()
-
 # Download NLTK data
 nltk.download('omw-1.4')
 nltk.download("punkt")
 nltk.download("wordnet")
 
-# Initialize lists and variables for data processing
+# Initialize NLTK's WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Load intents from JSON file
+with open('intents.json') as file:
+    intents = json.load(file)
+
+# Extract data from intents
 words = []
 classes = []
 documents = []
 ignore_words = ["?", "!"]
 
-# Read intents from intents.json
-data_file = open("intents.json").read()
-intents = json.loads(data_file)
-
-# Process intents and extract words and classes
-for intent in intents["intents"]:
-    for pattern in intent["patterns"]:
-        # Tokenize and lemmatize each word in the pattern
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        documents.append((w, intent["tag"]))
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
 
-        if intent["tag"] not in classes:
-            classes.append(intent["tag"])
-
-# Lemmatize words and remove duplicates
+# Lemmatize and remove duplicates
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 classes = sorted(list(set(classes)))
 
-# Debugging: Print some statistics about the data
-print(len(documents), "documents")
-print(len(classes), "classes", classes)
-print(len(words), "unique lemmatized words", words)
+# Save words and classes to files
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
-# Save words and classes to pickle files
-pickle.dump(words, open("words.pkl", "wb"))
-pickle.dump(classes, open("classes.pkl", "wb"))
-
-# Initialize training data
+# Create training data
 training = []
 output_empty = [0] * len(classes)
 
-# Create training data
 for doc in documents:
     bag = []
     pattern_words = doc[0]
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-
+    
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
     output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-
-    training.append([bag, output_row])
-
-# Shuffle training data and convert to NumPy arrays
-random.shuffle(training)
-train_x = np.array([x[0] for x in training])  # Extract the bag of words from training data
-train_y = np.array([x[1] for x in training])  # Extract the output rows from training data
-
-# Create a neural network model
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation="softmax"))
-
-# Compile the model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
-
-# Train the model
-hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
-
-# Save the trained model
-model.save("chatbot_model.h5")
-print("Model created and saved")
+    output_row[classes.index(doc[1])]
